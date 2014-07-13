ef272b7df45908f0862bb5b3114052fc71314355
alexmanzoli <alexsander_adm@hotmail.com>
2014-07-12 21:22:04 -0300

    criado classe de conexao banco de dados

---
+++ Sistema financero beta/Class banco.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sistema_financero_beta
+{
+    class Class_banco
+    {
+        //Cadastro de caminho do banco
+        private const string strCon = @"Data Source=.\SQLEXPRESS;AttachDbFilename='C:\Users\Term01\Documents\GitHub\Sistema-Financero\Sistema financero beta\SISTEMA.mdf';Integrated Security=True;Connect Timeout=30;User Instance=True";
+        private string vSql = "";
+        SqlConnection ObjCon = null;
+
+        //Verificação de conexao conectar
+        private bool Conectar()
+        {
+            ObjCon = new SqlConnection(strCon);
+            try
+            {
+                ObjCon.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        
+        //verificação de conexao desconectar
+        private bool desconectar()
+        {
+           if (ObjCon.State != ConnectionState.Closed)
+           {
+           ObjCon.Close();
+           ObjCon.Dispose();
+               return true;
+           }
+           else
+           {
+           ObjCon.Dispose();
+               return false;
+           }
+            }
+
+        //Clsse de inserir dados no banco 
+        public bool Insert()
+        {
+            if (this.Conectar())
+            {
+            }
+            {
+              try
+                }
+            
+                catch()
+                    {
+                    
+                    }
+                finally
+                {
+                this.desconectar();
+                }
+
+        //Clsse de Update dados no banco 
+        public bool Update()
+        { 
+        
+        }
+
+        //Clsse de deletar dados no banco 
+        public bool Delete()
+        { 
+        }
+
+        //Clsse de Lista Grid dados no banco 
+        public DataTable ListaGrid()
+        { 
+        
+        }
+
+        //Clsse de Pesquisa dados no banco 
+        public DataTable Pesquisar()
+        {
+
+        }
+
+        }
+    }
--- Sistema financero beta/FSIG1_CADEMP.cs
+++ Sistema financero beta/FSIG1_CADEMP.cs
@@ -8,6 +8,7 @@ using System.Windows.Forms;
 using System.Data.SqlClient;
 using System.Data;
 
+
 namespace Sistema_financero_beta
 {
     public partial class FSIG1_CADEMP : Form
@@ -41,10 +42,11 @@ namespace Sistema_financero_beta
         private void button1_Click(object sender, EventArgs e)
         {
             string conexao = @"Data Source=.\SQLEXPRESS;AttachDbFilename='C:\Users\Term01\Documents\GitHub\Sistema-Financero\Sistema financero beta\SISTEMA.mdf';Integrated Security=True;Connect Timeout=30;User Instance=True";
+            
             SqlConnection con = new SqlConnection (conexao);
             SqlCommand cmd = new SqlCommand ("insert into CAD_EMP(CSI_ID, CSI_NOME, CSI_NOME_FAM, CSI_END, CSI_NUM, CSI_COMPL, CSI_BAIRRO, CSI_UF, CSI_CIDADE, CSI_TEL, CSI_FAX, CSI_CNPJ, CSI_INCRI_ES, CSI_INCRI_MU, CSI_EMAIL, CSI_ARQUIVAMENTO, CSI_CO_ATIVI) Values ('" +textBox1.Text+"', '"+textBox2.Text+"', '"+textBox3.Text+"', '"+textBox4.Text+"', '"+textBox5.Text+"', '"+textBox6.Text+"', '"+textBox7.Text+"', '"+textBox8.Text+"', '"+textBox9.Text+"', '"+textBox10.Text+"', '"+textBox11.Text+"', '"+textBox12.Text+"', '"+textBox13.Text+"', '"+textBox14.Text+"', '"+textBox15.Text+"', '"+textBox16.Text+"')",con);
             con.Open();
-            cmd.ExecuteReader();
+            cmd.ExecuteNonQuery();
             con.Close();
            
   
